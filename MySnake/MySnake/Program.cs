09764b8b393cf50cfc311e125f49162d64bb53ad
ahella <he11owin@rambler.ru>
2015-10-15 14:20:51 +0300

    Добавили список

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -15,14 +15,34 @@ namespace MySnake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-          
+
+            List<int> numList = new List<int>();
+            numList.Add( 0 );
+            numList.Add( 1 );
+            numList.Add( 2 );
+
+            int x = numList[ 0 ];
+            int y = numList[ 1 ];
+            int z = numList[ 2 ];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add( p1 );
+            pList.Add( p2 );
+
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char sym)
+     /*   static void Draw(int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
-        }
+        }*/
     }
 }
