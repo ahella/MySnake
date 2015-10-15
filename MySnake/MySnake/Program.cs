40b0a0c30ae45ad0bdd71d03fd614057cf368841
ahella <he11owin@rambler.ru>
2015-10-15 15:39:31 +0300

    добавлен класс Snake

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -24,8 +24,9 @@ namespace MySnake
             rightline.Drow();
 
             // отрисовка точек
-            Point p1 = new Point(4, 5, '*');
-            p1.Draw();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake( p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
         }
